8f52e3a516b1dea332de0988feb0f088c607d43e
ewqoewqieiwqiewqiewq <hologts27@gmail.com>
2026-05-29 15:14:22 -0300

    Update SanAurie.lua

--- SanAurie.lua
+++ SanAurie.lua
@@ -1,1 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/921ef20181de9bb96cc98386ac927dea58fefcb4f36ffd77f9ebe79472368c28/download"))()
